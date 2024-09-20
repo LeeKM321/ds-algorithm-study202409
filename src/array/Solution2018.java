d4025e293ff4185130c5597775998a7a00d38ed2
LeeKM321 <stephen4951@gmail.com>
2024-09-20 18:11:55 +0900

    투 포인터 알고리즘 개념 및 문제 풀이

---
+++ src/array/Solution2018.java
@@ -0,0 +1,55 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution2018 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+
+        int start = 0, end = 0, count = 0, sum = 1;
+
+        while (end < N) {
+            if (sum == N) {
+                count++;
+                end++;
+                if (end < N) sum += arr[end];
+            } else if (sum > N) {
+                sum -= arr[start];
+                start++;
+            } else {
+                end++;
+                sum += arr[end];
+            }
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/array/TwoPointer.java
@@ -0,0 +1,53 @@
+package array;
+
+import javax.lang.model.SourceVersion;
+
+public class TwoPointer {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+        int targetSum = 5;
+        int start = 0, end = 0, sum = 1, count = 0;
+
+        while (end < arr.length) {
+
+            System.out.println("sum = " + sum);
+
+            if (sum == targetSum) {
+                count++;
+                end++;
+                if (end < arr.length) sum += arr[end];
+            } else if (sum > targetSum) { // 합이 목표값보다 크다면
+                sum -= arr[start];
+                start++;
+            } else { // 합이 목표값보다 작다면
+                sum += arr[++end];
+            }
+
+            System.out.println("start = " + start);
+            System.out.println("end = " + end);
+            System.out.println("after sum = " + sum);
+            System.out.println("count = " + count);
+            System.out.println("----------------------------------");
+
+
+        }
+
+
+        System.out.println("부분합이 " + targetSum + "인 구간의 수: " + count);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
